2656ea49d8313ee21fced3887bd620efa046662e
srikanthpragada <srikanthpragada@gmail.com>
2019-08-08 16:52:21 +0530

    08-AUG

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,48 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string Index()
+        {
+            return "<h1>Hello!</h1>";
+        }
+
+        public ActionResult Wish(string name)
+        {
+            var hours = DateTime.Now.Hour;
+            String msg = "Good Evening!";
+
+            if (hours < 12)
+                msg = "Good Morning!";
+            else
+                if (hours < 17)
+                msg = "Good Afternoon!";
+
+            if (name == null)    // param name is missing
+                name = "Guest";
+
+            ViewBag.Message = name + " , " + msg;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            var c = new Contact
+            {
+                Name = "Mike",
+                Email = "mike@gmail.com",
+                Mobile = "3934934343"
+            };
+
+            return View(c); // Sending model to view 
+        }
+    }
+}
