bb4595c3c61f320de349637786a0a863e95cb01f
srikanthpragada <srikanthpragada@gmail.com>
2019-08-13 08:58:07 +0530

    13-AUG

---
+++ Controllers/CacheController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class CacheController : Controller
+    {  
+        [OutputCache(Duration = 60, VaryByParam ="*")]
+        public ActionResult Index()
+        {
+            DateTime now = DateTime.Now;
+            return View(now);
+        }
+    }
+}
