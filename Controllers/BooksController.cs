77f2ef20caeacff7ac0ba88c29203b28f2cb7dd7
srikanthpragada <srikanthpragada@gmail.com>
2019-08-14 08:56:07 +0530

    14-AUG

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class BooksController : Controller
+    {
+         
+        public string Index()
+        {
+            return "<h1>Books List </h1>";
+        }
+
+        [Authorize]
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace mvcdemo.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string password)
+        {
+            if (password == "123")
+            {
+                FormsAuthentication.SetAuthCookie("guest", false);
+                return RedirectToAction("Add", "Books");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Password!";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login","User");
+
+        }
+    }
+}
