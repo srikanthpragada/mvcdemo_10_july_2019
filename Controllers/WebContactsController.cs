b9d67d510825590b657e7a169427ed7e7769b2ba
srikanthpragada <srikanthpragada@gmail.com>
2019-08-23 09:28:47 +0530

    23-AUG

---
+++ Controllers/WebContactsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using mvcdemo.Models;
+
+namespace mvcdemo.Controllers
+{
+    public class WebContactsController : ApiController
+    {
+        private ContactsContext db = new ContactsContext();
+
+        // GET: api/WebContacts
+        public IQueryable<Contact> GetContacts()
+        {
+            return db.Contacts;
+        }
+
+        // GET: api/WebContacts/5
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult GetContact(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        // PUT: api/WebContacts/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutContact(int id, Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/WebContacts
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult PostContact(Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Contacts.Add(contact);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = contact.Id }, contact);
+        }
+
+        // DELETE: api/WebContacts/5
+        [ResponseType(typeof(Contact))]
+        public IHttpActionResult DeleteContact(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                db.Contacts.Remove(contact);
+                db.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(contact);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContactExists(int id)
+        {
+            return db.Contacts.Count(e => e.Id == id) > 0;
+        }
+    }
+}
